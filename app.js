a0f94930d9d2f0a35b27aa5a8805915e1af29e55
Adlet Kurentaev <kurentaev@gmail.com>
2022-11-03 11:14:41 +0600

    Добавлено решение 2 задания.

--- app.js
+++ app.js
@@ -12,3 +12,22 @@ function productReview(products) {
 }
 
 productReview(products)
+
+
+// Задание 2
+let first = [1, 5, 12, 4, 3]
+let second = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+let third = [3, 10, 17]
+
+function calculateAverage(numbers) {
+    let sum = 0
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    let average = sum / numbers.length;
+    console.log(average)
+}
+
+calculateAverage(first)
+calculateAverage(second)
+calculateAverage(third)
